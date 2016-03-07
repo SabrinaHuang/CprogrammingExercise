ab4176f4324eec41d9c91697a0a298d3240705f7
SabrinaHuang <746982958@qq.com>
2016-03-07 17:32:45 +0800

    提交7-3的工程框架

---
+++ ex7-3/ex7-3/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  ex7-3
+//
+//  Created by Sabrina on 7/3/16.
+//  Copyright © 2016 yunda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <stdarg.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
+
+int minprintf( char *format, ... )
+{
+    va_list 
+    
+    return 0;
+}
