07dd0d5d6b2d04b0deadff9fc34bad3266d05056
SabrinaHuang <746982958@qq.com>
2016-03-08 01:25:31 +0800

    7-3的框架

--- ex7-3/ex7-3/main.m
+++ ex7-3/ex7-3/main.m
@@ -9,20 +9,61 @@
 #import <Foundation/Foundation.h>
 #include <stdarg.h>
 
+int minprintf( char *fmn, ... );
+
 int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        minprintf("%s %d", "adoegg", 3);
     }
     return 0;
 }
 
 
-int minprintf( char *format, ... )
+int minprintf( char *fmn, ... )
 {
-    va_list 
+    va_list ap; // 用于指向变量的指针
+    
+    // 以val结尾的变量均用于存放变元
+    char *p, *sval;
+    int ival;
+    double dval;
+    
+    // 获取变元
+    va_start(ap, fmn);
+    for (p = fmn; *p; ++p) // 指向第一个无名变量
+    {
+        // 读到 %时，开始解析
+        if ('%' != *p )
+        {
+            putchar(*p);
+            continue;
+        }
+        
+        // 根据格式控制字符决定变元的类型，用va_arg方法
+        switch (*++p)
+        {
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d",ival);
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f",dval);
+                break;
+            // va_arg用于取*p里的每一个字符
+            case 's':
+                for (sval = va_arg(ap, char *); *sval; ++sval)
+                    putchar(*sval);
+                break;
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+    va_end(ap);
+    
     
     return 0;
 }
